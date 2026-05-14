689c0776dd167b4c76208c1d7bdb4256cdce8b9b
Ahmad Azroun <ahmadahmody@gmail.com>
2026-05-15 01:30:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+from machine import Pin
+import time
+
+# -------- Erster Taster --------
+button1 = Pin(4, Pin.IN, Pin.PULL_UP)
+
+# Alte rote LED
+led1 = Pin(5, Pin.OUT)
+
+# Zusätzliche neue LED
+led_extra = Pin(22, Pin.OUT)
+
+# -------- Zweiter Taster --------
+button2 = Pin(19, Pin.IN, Pin.PULL_UP)
+led2 = Pin(21, Pin.OUT)
+
+# -------- Dritter Taster --------
+button3 = Pin(18, Pin.IN, Pin.PULL_UP)
+led_builtin = Pin(2, Pin.OUT)
+
+# Zustände
+state1 = 0
+state2 = 0
+state3 = 0
+
+last1 = 1
+last2 = 1
+last3 = 1
+
+while True:
+
+    # -------- Erster Taster --------
+    c1 = button1.value()
+
+    if last1 == 1 and c1 == 0:
+
+        state1 = 1 - state1
+
+        # Schaltet zwei LEDs gleichzeitig ein/aus
+        led1.value(state1)
+        led_extra.value(state1)
+
+    last1 = c1
+
+    # -------- Zweiter Taster --------
+    c2 = button2.value()
+
+    if last2 == 1 and c2 == 0:
+
+        state2 = 1 - state2
+        led2.value(state2)
+
+    last2 = c2
+
+    # -------- Dritter Taster --------
+    c3 = button3.value()
+
+    if last3 == 1 and c3 == 0:
+
+        state3 = 1 - state3
+        led_builtin.value(state3)
+
+    last3 = c3
+
+    time.sleep(0.05)
